15efacf0e67ffaa33865bf88d24e76f0d045878e
Vladislav Klimenko <sunsingerus@gmail.com>
2018-08-18 23:55:58 +0300

    win local node attch

--- win/05_way2_startupnode_start.bat
+++ win/05_way2_startupnode_start.bat
@@ -1,2 +1,1 @@
-geth --datadir c:\ethdatadir --ethash.dagdir c:\ethdatadir\ethash --verbosity 3 --mine --minerthreads=1 --etherbase=0xNNNNNNNNNNNNNNNNNNNNN
-
+geth --datadir c:\ethdatadir --ethash.dagdir c:\ethdatadir\ethash --verbosity 3 --mine --minerthreads=1 --etherbase=0x9992f4bedb829e79a0a407d9fb126794d2f910fd
