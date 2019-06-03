95a5b2450bfe7f5e17e54f754512b3963655b04b
Vladislav Klimenko <sunsingerus@gmail.com>
2019-06-03 12:45:20 +0300

    extract env vars

--- linux/02_init_chain.sh
+++ linux/02_init_chain.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir -p /home/user/ethdatadir
-cp genesis.json /home/user/ethdatadir
-geth --datadir /home/user/ethdatadir init /home/user/ethdatadir/genesis.json
+ETHDATADIR=~/ethdatadir
+
+mkdir -p "${ETHDATADIR}"
+cp ./genesis.json "${ETHDATADIR}"
+geth --datadir "${ETHDATADIR}" init "${ETHDATADIR}/genesis.json"
 
