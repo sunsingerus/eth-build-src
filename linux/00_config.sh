0a918b4ce4cee662a70e84ab7e0dcd5f834a4326
Vladislav Klimenko <sunsingerus@gmail.com>
2019-06-13 10:38:49 +0300

    config

--- linux/00_config.sh
+++ linux/00_config.sh
@@ -1,16 +1,30 @@
 
-# Setup paths
-ROOT_DIR=~
+#
+# Setup components version
+#
+
 #GOLANG_VERSION="1.10.3"
 #GOLANG_VERSION="1.12.5"
 GOLANG_VERSION="1.11.10"
-GOPATH_DIR="${ROOT_DIR}/gocode"
-GOROOT_DIR="${ROOT_DIR}/go"
 #ETH_VERSION="1.8.10"
 #ETH_VERSION="1.8.27"
 ETH_VERSION="1.8.26"
+
+#
+# Setup paths
+#
+ROOT_DIR=~
+GOPATH_DIR="${ROOT_DIR}/gocode"
+GOROOT_DIR="${ROOT_DIR}/go"
 ETHDATA_DIR=~/ethdatadir
 
-VERBOSITY=3
+#
+# Ethereum options
+#
+
+# Each private network has to have unique ID
 NETWORK_ID=15
+
+# Ethereum compoments verbosity
+VERBOSITY=3
 
